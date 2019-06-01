e9be3a3addda17fa2b75004aea3761af2fc6e60f
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-05-31 21:45:41 -0400

    Implemented animation.css to specific sections.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,19 +3,19 @@ $(document).ready(function(){
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
-        offset: '50%'
+        offset: '70%'
     });
 
     $('.js--wp-2').waypoint(function(direction) {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
-        offset: '50%'
+        offset: '70%'
     });
 
     $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
+        $('.js--wp-3').addClass('animated fadeInLeft');
     }, {
-        offset: '50%'
+        offset: '70%'
     });
 
     $('.js--wp-4').waypoint(function(direction) {
@@ -27,6 +27,12 @@ $(document).ready(function(){
       $('.js--wp-5').waypoint(function(direction) {
         $('.js--wp-5').addClass('animated fadeInRight');
     }, {
-        offset: '50%'
+        offset: '80%'
+    });
+
+      $('.js--wp-6').waypoint(function(direction) {
+        $('.js--wp-6').addClass('animated pulse slower');
+    }, {
+        offset: '60%'
     });
 });
